5b27d0897aad5455fc2818ce2127343970ac98c4
juniorha <36996697+juniorha@users.noreply.github.com>
2018-11-09 16:42:59 -0500

    Update Parrondo-s-Paradox-JuegoA.R

--- Parrondo-s-Paradox-JuegoA.R
+++ Parrondo-s-Paradox-JuegoA.R
@@ -5,7 +5,7 @@ experimentos <- 1#numero de experimentos
 v <- vector("numeric",length=experimentos)#vector para almacenar los resultados de los lanzamientos
 for(i in 1:experimentos){ 
 x <- sample(c(cara = 1, cruz = -1), nsim, replace = TRUE, prob = c(0.4995,0.5005))
-plot(1:nsim, cumsum(x), type="l", ylab=" Acumulado", xlab="NÃºmero de lanzamientos", ylim=c(-100,100))
+plot(1:nsim, cumsum(x), type="l", ylab=" Acumulado", xlab="Numero de lanzamientos", ylim=c(-100,100))
  
 #plot(cumsum(x), type = "l" , ylim = c(-100,100))
 abline(h=-1, lty=2, col="red", xlab="Numero de lanzamientos", ylim=c(-100,100)) 
